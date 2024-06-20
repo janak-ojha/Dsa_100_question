f5157f030bf4fade2f41a946c7c60ec327d53cad
janak-ojha <janakrajojha66@gmail.com>
2024-06-20 10:51:26 +0545

    palindrome

---
+++ String/CHecking_Vowel_cosonent.java
@@ -0,0 +1,13 @@
+package String;
+
+public class CHecking_Vowel_cosonent {
+    static void vow_co(String s){
+        
+    }
+
+    public static void main(String[] args) {
+        String s ="janak";
+        
+    }
+    
+}
---
+++ String/Palindrome_ottr_not.java
@@ -0,0 +1,32 @@
+package String;
+
+public class Palindrome_ottr_not {
+    static void Palindrome(String h)
+    { boolean Palindrom=true;
+        int f=0;
+        int l=h.length()-1;
+        while (f<l) {
+            if(h.charAt(l)!=h.charAt(f))
+            {
+                Palindrom=false;
+                
+            }
+            l--;
+            f++;
+            
+        }
+        if(Palindrom)
+        {
+            System.out.println("palindrome");
+        }
+        else{
+        System.out.println("not palindrome");
+    }
+    }
+    public static void main(String[] args) {
+        String h="1100011";
+        Palindrome(h);
+
+    }
+    
+}
