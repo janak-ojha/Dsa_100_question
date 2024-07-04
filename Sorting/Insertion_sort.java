eebf80537591c354717c8a7ae9987c054452d55c
janak-ojha <janakrajojha66@gmail.com>
2024-07-04 12:33:31 +0545

    insertion_sort

---
+++ Sorting/Insertion_sort.java
@@ -0,0 +1,39 @@
+package Sorting;
+
+public class Insertion_sort {
+    static void insert(int arr[])
+    {
+        for(int i=1;i<arr.length;i++)
+        {
+            int key =arr[i];
+            int j= i-1;
+            while (j>=0 && arr[j]>key) {
+                arr[j+1]=arr[j];
+                j--;
+
+                
+            }
+            arr[j+1]=key;
+        }
+    }
+    public static void main(String[] args) {
+        int arr[] ={1,4,3,5,2};
+        printe(arr);
+        System.out.println("before");
+        insert(arr);
+        printe(arr);
+        System.out.println("sfter");
+        
+
+    }
+    static void printe(int arr[])
+    {
+        for(int i=0;i<arr.length;i++)
+        {
+            System.out.print(arr[i]+" ");
+
+        }
+        System.out.println();
+    }
+    
+}
