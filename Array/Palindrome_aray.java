e20b466ab4322a01bdb38540281152beaf7321fb
janak-ojha <janakrajojha66@gmail.com>
2024-07-07 19:01:27 +0545

    palin

---
+++ Array/Palindrome_aray.java
@@ -0,0 +1,32 @@
+package Array;
+
+public class Palindrome_aray {
+    static void palin(int arr[])
+    {  boolean p = true;
+        int f=0;
+        int l= arr.length-1;
+        while (f<=l) {
+            if(arr[f] != arr[l])
+            {
+                p=false;
+            }
+           
+            f++;
+            l--;
+
+            
+        }
+        if(p)
+        {
+            System.out.println("is palindrome");
+        }
+        else{
+            System.out.println("not palindrome");
+        }
+    }
+    public static void main(String[] args) {
+        int arr[] ={1,2,3,4,3,2,1};
+        palin(arr);
+    }
+    
+}
---
+++ Prime_num.java
@@ -0,0 +1,68 @@
+public class Prime_num {
+
+    static boolean ispr(int a)
+    {
+        
+        if(a==1)
+        {
+            return false;
+
+        }
+        if(a==2 || a==3)
+        {
+            return true;
+        }
+        if(a%2==0 ||  a%3==0)
+        {
+            return false;
+        }
+        for(int i=5;i*i<=a;i=i+6)
+        {
+            if(a%i==0 || a%(i+2)==0)
+            {
+                return false;
+            }
+
+        }
+        return true;
+
+    }
+
+    static boolean pre(int n)
+    {
+        for(int i=2;i*i<n;i++)
+        {
+            if(n%i == 0)
+            {
+                return false;
+            }
+        }
+        return true;
+    }
+    static boolean pr(int n)
+    {
+        for(int i=2;i<n/2;i++)
+        {
+            if(n%i==0)
+            {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public static void main(String[] args) {
+        
+        System.out.println(ispr(24));
+        boolean res = ispr(24);
+        if(res)
+        {
+            System.out.println("it is prime");
+        }
+        else{
+            System.out.println("not prime");
+        }
+        
+    }
+    
+}
