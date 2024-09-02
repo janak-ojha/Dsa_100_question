e8d44f279c17864141f7d8603dc8979df600e3c0
janak-ojha <janakrajojha66@gmail.com>
2024-09-02 17:20:01 +0530

    leng_of_last_word

---
+++ Leet_code_Array/Length_of_last_word.java
@@ -0,0 +1,28 @@
+package Leet_code_Array;
+
+public class Length_of_last_word {
+    static int len(String s)
+    {
+        int length =0;
+        for(int i=s.length()-1;i>=0;i--)
+        {
+            if(s.charAt(i) == ' ')
+            {
+                if(length>0)
+                {
+                    break;
+                }
+            }
+            else{
+                length++;
+            }
+        }
+        return length;
+    }
+    public static void main(String[] args) {
+        String s = "janak raj ojhaa";
+        System.out.println(len(s));
+        
+    }
+    
+}
