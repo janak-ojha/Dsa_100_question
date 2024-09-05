74923826599be4fbc97e326994e7e12d964eecc1
janak-ojha <janakrajojha66@gmail.com>
2024-09-05 22:25:56 +0530

    hlo

---
+++ FIRSt_ten_prime.java
@@ -0,0 +1,26 @@
+public class FIRSt_ten_prime {
+    
+    static  boolean isprime(int n)
+    {
+        for(int i=2;i*i<=n;i++)
+        {
+            if(n%i == 0)
+            {
+                return false;
+            }
+
+        }
+        return true;
+    }
+    public static void main(String[] args) {
+       int n=2;
+       while (n<=20) {
+        if(isprime(n)){
+            System.out.println(n+" ");
+        }
+        n++;
+        
+       }
+        
+    }
+}
